035caf9b276a6d89d05ec9d7ed9486eb4e08304f
shukla93shashank <165426042+shukla93shashank@users.noreply.github.com>
2024-03-31 08:26:14 +0530

    Added Discovery Service feature

--- oder-service/src/main/java/com/productapplication/orderservice/orderservice/config/webClientConfig.java
+++ oder-service/src/main/java/com/productapplication/orderservice/orderservice/config/webClientConfig.java
@@ -1,6 +1,7 @@
 package com.productapplication.orderservice.orderservice.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,7 +10,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class webClientConfig {
 
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
     }
 }
